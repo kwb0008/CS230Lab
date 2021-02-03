76fcb9f9e0bdbedf89d6cdd999cc30b66bd5db8d
kwb0008 <kwb0008@mix.wvu.edu>
2021-02-02 20:45:50 -0500

    Lab2 Begin AboutUsPage

---
+++ index.php
@@ -0,0 +1,48 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>rjwberjbvwrebver</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.png">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kyle Breedlove</p>
+                    <p> Email: </p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.png">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kyle Breedlove</p>
+                    <p> Email: </p>
+                </div>
+            </div>
+        </div> <div class="column">
+            <div class="card">
+                <img src="images/3.png">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kyle Breedlove</p>
+                    <p> Email: </p>
+                </div>
+            </div>
+        </div> <div class="column">
+            <div class="card">
+                <img src="images/4.png">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kyle Breedlove</p>
+                    <p> Email: </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
