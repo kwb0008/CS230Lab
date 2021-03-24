ea6a83f94d5077cd637f3aaadad19e05043080b6
kwb0008 <kwb0008@mix.wvu.edu>
2021-03-24 17:15:29 -0400

    db

---
+++ cs230.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 24, 2021 at 05:12 PM
+-- Server version: 5.7.33-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs230`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `pid` int(11) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `descript` text NOT NULL,
+  `picpath` varchar(80) NOT NULL,
+  `upload_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`pid`, `title`, `descript`, `picpath`, `upload_date`, `rating`) VALUES
+(3, 'test1', 'asdax', '../gallery/60480ea3b0e802.68372640.jpeg', '2021-03-09 19:11:15', NULL),
+(4, 'test2', 'wefrec', '../gallery/60480eafe933f4.46395340.png', '2021-03-09 19:11:27', NULL),
+(5, 'test3', 'rrrgrvdv', '../gallery/60480ebfa87727.63353935.png', '2021-03-09 19:11:43', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `pid` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `profpic` varchar(50) NOT NULL DEFAULT '../images/default.jpeg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`pid`, `fname`, `uname`, `profpic`) VALUES
+(1, 'K', 'KB', '../images/default.jpeg'),
+(2, 'K', '123', '../profiles/6046b9b1a7e8f1.06436255.jpeg'),
+(3, '123', '123', '../profiles/6046b9b1a7e8f1.06436255.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `revid` int(11) NOT NULL COMMENT 'id for review',
+  `itemid` int(11) NOT NULL COMMENT 'id for item being reveiwed',
+  `uname` varchar(80) NOT NULL COMMENT 'user reviewing item',
+  `title` varchar(60) NOT NULL,
+  `reviewtext` text NOT NULL,
+  `revdate` datetime NOT NULL,
+  `ratingnum` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT 'at least 1 review'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`revid`, `itemid`, `uname`, `title`, `reviewtext`, `revdate`, `ratingnum`, `status`) VALUES
+(1, 3, '123', 'tj', 'tj', '2021-03-17 23:04:29', 5, 1),
+(2, 3, '123', 'tj', 'tj', '2021-03-17 23:05:17', 5, 1),
+(3, 3, '123', 'rfvweac', 'ewfwrg', '2021-03-17 23:50:49', 2, 1),
+(4, 3, '123', 'f', 'fverfec', '2021-03-17 23:50:58', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COMMENT='User database for setup and signin';
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `fname`, `lname`, `uname`, `password`, `email`) VALUES
+(3, '123', '123', '123', '$2y$10$LoBG0aXqwnxO0yOHbX.Rwe/4LxKr4j6o99yqRPSUxhpErlbbIRWe2', '1@1.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`revid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `revid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id for review', AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
